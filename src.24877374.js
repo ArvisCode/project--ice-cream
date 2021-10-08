432a0a385a34e5ce0e6dbc7ed1d56c26f78cd0c2
OlesiaLykhovyd <85580082+OlesiaLykhovyd@users.noreply.github.com>
2021-10-08 15:56:53 +0000

    Deploying to gh-pages from @ ArvisCode/project--ice-cream@54c804b16033c1224b6878ebde3bc53f2203ae35 🚀

--- src.24877374.js
+++ src.24877374.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/project--ice-cream/src.d99c3268.js.map
+//# sourceMappingURL=/project--ice-cream/src.24877374.js.map
