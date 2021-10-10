45d3d9a9e8a85386c5d12e43ac02ec0e0c29b2bc
Bohdan Strilets <bohdan.strilets@gmail.com>
2021-10-10 16:31:25 +0200

    feature/order-forms
    modal window and order form

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,11 +1,13 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
+    openModalBtnMobile: document.querySelector('[data-modal-open-mobile]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.openModalBtnMobile.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
